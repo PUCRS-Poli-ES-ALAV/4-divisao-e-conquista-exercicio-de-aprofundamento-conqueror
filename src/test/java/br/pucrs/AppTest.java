e71dfd2297476fcbd6a45b5677747b3ecf1d0f8b
Erik Vieira Suris <erik.suris@edu.pucrs.br>
2024-08-28 20:36:05 -0300

    Some testing

--- src/test/java/br/pucrs/AppTest.java
+++ src/test/java/br/pucrs/AppTest.java
@@ -9,10 +9,9 @@ public class AppTest
     @Test
     public void maxVal2Test()
     {
-        MaxVal2 greaterDC = new MaxVal2();
         long[] arr = {49,55,24,72,17};
 
-        long result = greaterDC.maxVal2(arr, 0, arr.length - 1);
+        long result = MaxVal2.maxVal2(arr, 0, arr.length - 1);
 
         assertEquals(72, result);
     }
